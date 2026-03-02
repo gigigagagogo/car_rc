f6de6dd274a8306db48d6d36e9a6e7ce11a4a3ba
gigigagagogo <robertbararu2@gmail.com>
2026-03-02 11:36:28 +0100

    implement all 4 motor logic

--- car_logic/src/main.cpp
+++ car_logic/src/main.cpp
@@ -2,22 +2,24 @@
 #include <WiFi.h>
 #include <SparkFun_TB6612.h>
 
-int PWMA = 33, PWMB = 32;
-int BIN2 = 15, BIN1 = 16;
-int AIN1 = 17, AIN2 = 18; 
-int STBY = 2; 
+int PWMA_1 = 33, PWMB_1 = 32, PWMA_2 = 26, PWMB_2 = 25;
+int BIN2_1 = 15, BIN1_1 = 16, BIN2_2 = 23, BIN1_2 = 22;
+int AIN1_1 = 17, AIN2_1 = 18, AIN1_2 = 19, AIN2_2 = 21; 
+int STBY_1 = 2, STBY_2 = 0; 
 int offset = 1;
-Motor motor1 = Motor(BIN1, BIN2, PWMB, offset, STBY);
-Motor motor2 = Motor(AIN1, AIN2, PWMA, offset, STBY);
+Motor motor1 = Motor(BIN1_1, BIN2_1, PWMB_1, offset, STBY_1);
+Motor motor2 = Motor(AIN1_1, AIN2_1, PWMA_1, offset, STBY_1);
+
+Motor motor3 = Motor(BIN1_2, BIN2_2, PWMB_2, offset, STBY_2);
+Motor motor4 = Motor(AIN1_2, AIN2_2, PWMA_2, offset, STBY_2);
 
 const char* ssid = WIFI_SSID;
 const char* password = WIFI_PASS;
 
-IPAddress local_IP(192, 168, 1, 100);
-IPAddress gateway(192, 168, 1, 1);
+IPAddress local_IP(10, 87, 202, 50);
+IPAddress gateway(10, 87, 202, 1);
 IPAddress subnet(255, 255, 255, 0);
-IPAddress primaryDNS(8, 8, 8, 8);
-IPAddress secondaryDNS(8, 8, 4, 4);
+
 
 WiFiServer server(3333); 
 void parseCommands(String &line, int &throttle, int &ster);
@@ -26,10 +28,10 @@ void setup() {
   Serial.begin(115200);
   
   // Configura IP statico
-  if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
+  if (!WiFi.config(local_IP, gateway, subnet)) {
     Serial.println("STA Failed to configure");
   }
-  
+
   Serial.print("Connecting to: ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
@@ -37,6 +39,8 @@ void setup() {
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
+    Serial.print(" Status: ");
+    Serial.println(WiFi.status());
   }
   
   Serial.println("");
@@ -65,6 +69,9 @@ void loop() {
           line = "";
           motor2.drive(throttle);
           motor1.drive(throttle);
+
+          motor3.drive(throttle);
+          motor4.drive(throttle);
         }
       }
       delay(1);
